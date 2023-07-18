663a419cbe09d23f0e3cb4ba02c55b7e4ee9d95d
ghada <ghadaemad011@gmail.com>
2023-07-18 19:14:57 +0300

    easy type

---
+++ C++/Hashing/0392. Is Subsequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int count=0;
+        for(int i=0;i<t.size();i++){
+            if(t[i]==s[count])count++;
+        }
+        return count==s.size();
+    }
+};
